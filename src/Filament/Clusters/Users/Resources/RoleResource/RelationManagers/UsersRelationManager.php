0adde30877170d2319fd625fa02de86ed05aab17
carly <carly.cheung@solutionforest.net>
2024-09-05 13:14:16 +0800

    fic User table on role page

--- src/Filament/Clusters/Users/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ src/Filament/Clusters/Users/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -10,21 +10,25 @@ use SolutionForest\InspireCms\Models\Contracts\User;
 class UsersRelationManager extends RelationManager
 {
     protected static string $relationship = 'users';
+    protected static ?string $recordTitleAttribute = 'name';
     protected static ?string $icon = 'heroicon-o-users';
 
     public function table(Table $table): Table
     {
         return $table
+            ->inverseRelationship('roles')
             ->columns([
                 Tables\Columns\ImageColumn::make('avatar')
                     ->label(' ')
                     ->circular()
                     ->getStateUsing(fn (User $record) => $record->getFilamentAvatarUrl() ?? filament()->getUserAvatarUrl($record)),
                 Tables\Columns\TextColumn::make('name')
-                    ->label(__('inspirecms::inspirecms.name')),
+                    ->label(__('inspirecms::inspirecms.name'))
+                    ->width('90%'),
             ])
             ->headerActions([
-                Tables\Actions\AssociateAction::make(),
+                Tables\Actions\AttachAction::make()
+                    ->preloadRecordSelect(),
             ])
             ->actions([
                 Tables\Actions\DetachAction::make(),
