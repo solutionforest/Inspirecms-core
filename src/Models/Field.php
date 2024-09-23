59af86fa1a7f3dac71f88ead54039897b1d22745
Carly <carly.cheung@solutionforest.net>
2024-09-23 11:44:26 +0800

    ensure config of field not null

--- src/Models/Field.php
+++ src/Models/Field.php
@@ -4,4 +4,14 @@ namespace SolutionForest\InspireCms\Models;
 
 use SolutionForest\FilamentFieldGroup\Models\Field as BaseModel;
 
-class Field extends BaseModel {}
+class Field extends BaseModel 
+{
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function (self $model) {
+            $model->config ??= [];
+        });
+    }
+}
