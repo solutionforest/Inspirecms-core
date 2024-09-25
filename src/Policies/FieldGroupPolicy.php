1e13b1c887ad6f2842661d0f61f3fa6518b87d4f
carly <carly.cheung@solutionforest.net>
2024-09-25 17:50:36 +0800

    add FieldGroupPolicy

---
+++ src/Policies/FieldGroupPolicy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SolutionForest\InspireCms\Policies;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use SolutionForest\FilamentFieldGroup\Models\Contracts\FieldGroup;
+use SolutionForest\InspireCms\Base\BasePolicy;
+use SolutionForest\InspireCms\Models\Contracts\User;
+
+class FieldGroupPolicy extends BasePolicy
+{
+    /**
+     * @param  Authenticatable|User|Model  $fieldGroup
+     * @return bool
+     */
+    public function create($fieldGroup)
+    {
+        return $user?->can(static::guessPermissionName(__FUNCTION__, FieldGroup::class));
+    }
+
+    /**
+     * @param  Authenticatable|User|Model  $fieldGroup
+     * @return bool
+     */
+    public function viewAny($fieldGroup)
+    {
+        return $user?->can(static::guessPermissionName(__FUNCTION__, FieldGroup::class));
+    }
+
+    /**
+     * @param  Authenticatable|User|Model  $fieldGroup
+     * @param  FieldGroup|Model  $fieldGroup
+     * @return bool
+     */
+    public function update($user, FieldGroup $fieldGroup)
+    {
+        return $user?->can(static::guessPermissionName(__FUNCTION__, FieldGroup::class));
+    }
+
+    /**
+     * @param  Authenticatable|User|Model  $fieldGroup
+     * @param  FieldGroup|Model  $fieldGroup
+     * @return bool
+     */
+    public function delete($user, FieldGroup $fieldGroup)
+    {
+        return $user?->can(static::guessPermissionName(__FUNCTION__, FieldGroup::class));
+    }
+
+    /**
+     * @param  Authenticatable|User|Model  $fieldGroup
+     * @param  null|FieldGroup|Model  $fieldGroup
+     * @return bool
+     */
+    public function replicate($user, $fieldGroup = null)
+    {
+        return $user?->can(static::guessPermissionName(__FUNCTION__, FieldGroup::class));
+    }
+}
