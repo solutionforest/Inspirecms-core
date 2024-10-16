80ed9fdf5b27ed7f1838e1ec1e8d29b20030fd7e
carly <carly.cheung@solutionforest.net>
2024-10-16 19:04:16 +0800

    update media library page

--- resources/views/filament/pages/media-library.blade.php
+++ resources/views/filament/pages/media-library.blade.php
@@ -1,3 +1,15 @@
+@php
+    $isMultiple = false;
+    $formConfig = [
+        'upload' => [
+            'collap_open' => true,
+        ],
+    ];
+
+@endphp
 <x-filament-panels::page>
-    @livewire('inspirecms-support::media-library', ['isMultiple' => false])
+    <livewire:inspirecms-support::media-library
+        :isMultiple="$isMultiple"
+        :formConfig="$formConfig"
+    />
 </x-filament-panels::page>
