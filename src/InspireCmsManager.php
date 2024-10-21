2773036725f90182b6866729164a60f6713c4499
carly <carly.cheung@solutionforest.net>
2024-10-21 12:20:48 +0800

    nav

--- src/InspireCmsManager.php
+++ src/InspireCmsManager.php
@@ -153,7 +153,7 @@ class InspireCmsManager
         // }
 
         // temp: current get root
-        $nav = InspireCmsConfig::getNavigationModelClass()::root()->navType($type)->get();
+        $nav = InspireCmsConfig::getNavigationModelClass()::with('content')->root()->navType($type)->get();
 
         return $nav->map(fn ($item) => $item->toDto($locale ?? app()->getLocale()))->all();
     }
