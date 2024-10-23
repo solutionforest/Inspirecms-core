d67f250df649bc65d3f8c4012b1317cd12c10897
carly <carly.cheung@solutionforest.net>
2024-10-23 11:22:23 +0800

    fix method "attemptToGetUrl" to allow "index" page

--- src/Helpers/FilamentResourceHelper.php
+++ src/Helpers/FilamentResourceHelper.php
@@ -8,43 +8,48 @@ class FilamentResourceHelper
      * Attempts to generate a URL for a given resource based on the provided actions and parameters.
      *
      * @param  string  $resource  The resource for which the URL is being generated.
-     * @param  array|string  $actions  An array of actions to be considered for URL generation.
+     * @param array|string $pages The pages associated with the resource.
      * @param  array  $parameters  An array of parameters to be included in the URL.
      * @param  bool  $autorizeAction  A flag indicating whether the action should be authorized.
      * @return ?string The generated URL if successful, or null if the URL could not be generated.
      */
-    public static function attemptToGetUrl(string $resource, array | string $actions, array $parameters, bool $autorizeAction): ?string
+    public static function attemptToGetUrl(string $resource, array | string $pages, array $parameters, bool $autorizeAction): ?string
     {
         $url = null;
 
-        if (is_string($actions)) {
-            $actions = [$actions];
+        if (is_string($pages)) {
+            $pages = [$pages];
         }
 
         try {
-            foreach ($actions as $action) {
+            foreach ($pages as $page) {
                 if (filled($url)) {
                     continue;
                 }
 
-                if (! $resource::hasPage($action)) {
+                if (! $resource::hasPage($page)) {
                     continue;
                 }
 
                 $record = $parameters['record'] ?? null;
+
                 if ($autorizeAction) {
 
+                    $action = $page === 'index' ? 'access' : $page;
+
                     $method = (string) str($action)->studly()->prepend('can');
+
                     if (! $resource::{$method}($record)) {
                         continue;
                     }
                 }
 
-                $url = $resource::getUrl($action, $parameters);
+                $url = $resource::getUrl($page, $parameters);
             }
 
         } catch (\Throwable $th) {
             //
+            dd($th);
         }
 
         return $url;
