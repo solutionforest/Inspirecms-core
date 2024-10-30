4ea2e71ca16ae3f47b4c2a3305bf12f9d861e906
carly <carly.cheung@solutionforest.net>
2024-10-30 18:09:20 +0800

    move packages location to build asset with support package

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     content: [
         './src/Filament/**/*.php',
         './resources/views/**/*.blade.php',
+        '../../packages/inspirecms-*/resources/views/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
 }
