1b76512182c639b73c7f8dd3bf2b1d56f62c9a88
cklei-carly <cklei-carly@users.noreply.github.com>
2024-11-20 10:38:31 +0000

    Fix styling

--- routes/cms.php
+++ routes/cms.php
@@ -6,7 +6,7 @@ Route::prefix(config('insiprecms.filament.path', 'cms'))->group(function () {
 
     // Redirect to the login page if the user is not authenticated for session authentication
     Route::get('/login-redirect', function () {
-        
+
         $url = filament()->getPanel(config('insiprecms.filament.panel_id', 'cms'))?->getLoginUrl() ?? ('/' . config('insiprecms.filament.path', 'cms') . '/login');
 
         return redirect()->intended($url);
