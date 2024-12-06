9c6e39a9921415d7b19199a61c036e32330a0bfc
carly <carly.cheung@solutionforest.net>
2024-12-06 16:40:27 +0800

    Disable link on filament peek modal

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,22 @@
 import "./tour-guide";
+
+// Function to disable links within the iframe
+function disableIframeLinks(iframe) {
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const links = iframeDocument.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+    });
+}
+
+document.addEventListener('peek:modal-opened', () => {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+        iframe.addEventListener('load', () => {
+            disableIframeLinks(iframe);
+        });
+    }
+});
