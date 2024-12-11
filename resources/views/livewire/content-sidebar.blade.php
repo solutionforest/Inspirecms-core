3a1903b684b66c3d98c0cfcbd091b6367e425c5b
carly <carly.cheung@solutionforest.net>
2024-12-11 18:20:09 +0800

    fix: content sidebar styles

--- resources/views/livewire/content-sidebar.blade.php
+++ resources/views/livewire/content-sidebar.blade.php
@@ -25,7 +25,7 @@
                     'text-gray-400 hover:text-gray-500 focus-visible:ring-primary-600 dark:text-gray-300 dark:hover:text-gray-100 dark:focus-visible:ring-primary-500',
                 ])
                     x-on:click="sidebarExpanded = !sidebarExpanded" 
-                    aria-label="Expand content sidebar"
+                    title="Expand content sidebar"
                 >
                     <span class="sr-only">Expand content sidebar</span>
                     <svg class="w-5 h-5" x-show="sidebarExpanded" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.97 15V9C21.97 4 19.97 2 14.97 2H8.96997C3.96997 2 1.96997 4 1.96997 9V15C1.96997 20 3.96997 22 8.96997 22H14.97C19.97 22 21.97 20 21.97 15Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7.96997 2V22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14.97 9.43994L12.41 11.9999L14.97 14.5599" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
