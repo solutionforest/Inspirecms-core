9e302cd3b9286e9e265b97c1ff5d5ea13a16bf39
carly <carly.cheung@solutionforest.net>
2025-01-06 18:23:56 +0800

    add validation attribute for user

--- resources/lang/zh_TW/resources/user.php
+++ resources/lang/zh_TW/resources/user.php
@@ -3,35 +3,46 @@
 return [
     'name' => [
         'label' => '姓名',
+        'validation_attribute' => '姓名',
     ],
     'email' => [
         'label' => '電子郵件地址',
+        'validation_attribute' => '電子郵件地址',
     ],
     'roles' => [
         'label' => '角色',
+        'validation_attribute' => '角色',
     ],
     'password' => [
         'label' => '密碼',
+        'validation_attribute' => '密碼',
     ],
     'password_confirmation' => [
         'label' => '確認密碼',
+        'validation_attribute' => '確認密碼',
     ],
     'last_logged_in_at' => [
         'label' => '最後登入時間',
+        'validation_attribute' => '最後登入時間',
     ],
     'failed_login_attempt' => [
         'label' => '登入失敗次數',
+        'validation_attribute' => '登入失敗次數',
     ],
     'last_lockouted_at' => [
         'label' => '最後鎖定時間',
+        'validation_attribute' => '最後鎖定時間',
     ],
     'email_confirmed_at' => [
         'label' => '電子郵件確認時間',
+        'validation_attribute' => '電子郵件確認時間',
     ],
     'avatar' => [
         'label' => '頭像',
+        'validation_attribute' => '頭像',
     ],
     'preferred_language' => [
         'label' => '偏好語言',
+        'validation_attribute' => '偏好語言',
     ],
 ];
