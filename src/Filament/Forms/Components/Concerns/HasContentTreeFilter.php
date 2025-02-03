3ffed27a4c5c3f7e6464d0133c1a121e33758a89
carly <carly.cheung@solutionforest.net>
2025-02-03 16:29:08 +0800

    ensure filter for ContentTree

--- src/Filament/Forms/Components/Concerns/HasContentTreeFilter.php
+++ src/Filament/Forms/Components/Concerns/HasContentTreeFilter.php
@@ -2,6 +2,8 @@
 
 namespace SolutionForest\InspireCms\Filament\Forms\Components\Concerns;
 
+use SolutionForest\InspireCms\Filament\Forms\Components\ContentTree\Filter\BaseFilter;
+
 trait HasContentTreeFilter
 {
     public array $filters = [];
@@ -13,31 +15,49 @@ trait HasContentTreeFilter
         return $this;
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function whereKey($key): static
     {
         return $this->where('id', '==', $key);
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function whereKeyNot($key): static
     {
         return $this->whereNot('id', $key);
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function whereIn($key, $values): static
     {
         return $this->where($key, 'in', $values);
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function whereNotIn($key, $values): static
     {
         return $this->where($key, 'not in', $values);
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function whereNot($key, $value): static
     {
         return $this->where($key, 'not', $value);
     }
 
+    /**
+     * @param string | BaseFilter $key
+     */
     public function where($key, $operator = null, $value = null): static
     {
         return $this->filter([
