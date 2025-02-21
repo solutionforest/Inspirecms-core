a8c65a2f8336e890dd8e6492013bd6caa47f5d4d
carly <carly.cheung@solutionforest.net>
2025-02-21 12:43:27 +0800

    Add back missing models and migrations for testcase

---
+++ tests/TestModels/ContentLock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SolutionForest\InspireCms\Tests\TestModels;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ContentLock extends \SolutionForest\InspireCms\Models\ContentLock
+{
+    use HasFactory;
+}
+
---
+++ tests/TestModels/Export.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SolutionForest\InspireCms\Tests\TestModels;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Export extends \SolutionForest\InspireCms\Models\Export
+{
+    use HasFactory;
+}
+
