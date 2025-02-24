0e65de71b95622ba9bec687e2dff24cc35d75ce9
carly <carly.cheung@solutionforest.net>
2025-02-24 13:47:01 +0800

    Update template instruction format

--- resources/views/instructions/page-component-instructions.blade.php
+++ resources/views/instructions/page-component-instructions.blade.php
@@ -2,15 +2,6 @@
 $plaintext = '<x-cms-template :content="$content" type="page">
     Your content here
 </x-cms-template>';
-$instructions = str($plaintext)
-    ->explode("\r\n")
-    ->map(fn ($line) => str($line)
-        ->replaceMatches('/\s\s+/', '&nbsp;&nbsp;')
-        ->inlineMarkdown([
-            'html_input' => 'escape',
-            'allow_unsafe_links' => false,
-        ])
-);
 @endphp
 
 <x-filament::section
@@ -21,13 +12,17 @@ $instructions = str($plaintext)
         {{ trans('inspirecms::resources/template.page_component_instructions.label') }}
     </x-slot>
     <div class="flex gap-x-2 justify-between">
-        <div class="flex-1 relative font-mono text-xs text-wrap text-clip overflow-x-auto">
-            @foreach ($instructions as $line)
-                <div class="line whitespace-nowrap">
-                    {!! $line !!}
-                </div>
-            @endforeach
-        </div>
+        <pre class="overflow-auto m-0 p-0">
+            <code class="text-xs text-mono break-words">
+                @php
+                    $displayText = str($plaintext)
+                        ->trim() // Remove whitespace
+                        ->prepend(PHP_EOL) // Add a newline at the start
+                        ->toString();
+                @endphp
+                {{ $displayText }}
+            </code>
+        </pre>
 
         <x-inspirecms::buttons.copy-button
             :plaintext="$plaintext"
