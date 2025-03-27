da0bed5e9ebef2971722a14ae2a5dd01d0173903
cklei-carly <68525320+cklei-carly@users.noreply.github.com>
2025-03-27 09:26:28 +0000

    Fix styling

--- src/Helpers/ImportDataHelper.php
+++ src/Helpers/ImportDataHelper.php
@@ -226,14 +226,14 @@ class ImportDataHelper
                                     type: 'text',
                                     config: [],
                                 ),
-                                ], [
+                            ], [
                                 new Entities\Field(
                                     slug: 'field-5',
                                     label: 'Field 5',
                                     type: 'text',
                                     config: [],
                                 ),
-                                ],
+                            ],
                         ])
                             ->map(fn (array $fields, $index) => new Entities\FieldGroup(
                                 slug: $itemSlug,
