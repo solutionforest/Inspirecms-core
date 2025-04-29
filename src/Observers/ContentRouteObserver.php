9311cd98731363e6fcdea8c47b608fb25afe54f5
carly <carly.cheung@solutionforest.net>
2025-04-29 14:04:26 +0800

    fix: default route of content doestn't update if slug changed

--- src/Observers/ContentRouteObserver.php
+++ src/Observers/ContentRouteObserver.php
@@ -3,7 +3,10 @@
 namespace SolutionForest\InspireCms\Observers;
 
 use Illuminate\Database\Eloquent\Model;
+use SolutionForest\InspireCms\Events\Content\UpsertRoute;
 use SolutionForest\InspireCms\Facades\InspireCms;
+use SolutionForest\InspireCms\Factories\ContentSegmentFactory;
+use SolutionForest\InspireCms\Models\Contracts\Content;
 use SolutionForest\InspireCms\Models\Contracts\ContentRoute;
 
 class ContentRouteObserver
@@ -15,6 +18,35 @@ class ContentRouteObserver
     public function created($model)
     {
         $this->clearCached();
+    }
+
+    /**
+     * @param  ContentRoute&Model  $model
+     * @return void
+     */
+    public function saved($model)
+    {
+        $content = $model->content;
+        if ($content) {
+
+            $segmentProvider = ContentSegmentFactory::create();
+
+            $content->children()->get()->load('routes')->each(function (Content|Model $child) use ($segmentProvider) {
+            
+                $uri = $segmentProvider->getSegment($child);
+                
+                $currentRoutes = collect($child->routes->where('is_default_pattern', true))
+                    ->map(fn (Model $model) => $model->toArray())
+                    ->map(function (array $data) use ($uri) {
+                        $data['uri'] = $uri;
+    
+                        return $data;
+                    })
+                    ->all();
+    
+                event(new UpsertRoute($child->withoutRelations(), $currentRoutes));
+            });
+        }
     }
 
     /**
