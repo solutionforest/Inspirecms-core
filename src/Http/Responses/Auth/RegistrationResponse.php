b144195ef085e76b455d8fa08c32cbff76146611
carly <carly.cheung@solutionforest.net>
2025-05-29 16:50:42 +0800

    Fix: avoid redirect to cms panel if reg new user on other panel

--- src/Http/Responses/Auth/RegistrationResponse.php
+++ src/Http/Responses/Auth/RegistrationResponse.php
@@ -3,14 +3,20 @@
 namespace SolutionForest\InspireCms\Http\Responses\Auth;
 
 use Filament\Http\Responses\Auth\Contracts\RegistrationResponse as RegistrationResponseContract;
+use Filament\Http\Responses\Auth\RegistrationResponse as FilamentRegistrationResponse;
 use Illuminate\Http\RedirectResponse;
 use Livewire\Features\SupportRedirects\Redirector;
 use SolutionForest\InspireCms\Facades\InspireCms;
+use SolutionForest\InspireCms\InspireCmsConfig;
 
-class RegistrationResponse implements RegistrationResponseContract
+class RegistrationResponse extends FilamentRegistrationResponse implements RegistrationResponseContract
 {
     public function toResponse($request): RedirectResponse | Redirector
     {
-        return redirect()->intended(InspireCms::getImportDataUrl() ?? filament()->getUrl());
+        if (filament()->getCurrentPanel()?->getId() === InspireCmsConfig::getPanelId() && ($importDataUrl = InspireCms::getImportDataUrl())) {  
+            return redirect()->intended($importDataUrl);
+        }
+
+        return parent::toResponse($request);
     }
 }
