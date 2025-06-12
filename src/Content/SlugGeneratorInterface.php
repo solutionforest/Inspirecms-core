9fcc9e30aca88e3984288ea7f2c1d3619af9008f
cklei-carly <68525320+cklei-carly@users.noreply.github.com>
2025-06-12 09:29:51 +0000

    Fix styling

--- src/Content/SlugGeneratorInterface.php
+++ src/Content/SlugGeneratorInterface.php
@@ -7,9 +7,9 @@ interface SlugGeneratorInterface
     /**
      * Generates a slug from the provided text.
      *
-     * @param string $text The text to generate a slug from.
-     * @param string $language The language of the text, default is auto-detect.
-     * @param string $separator The separator to use between words, default is '-'.
+     * @param  string  $text  The text to generate a slug from.
+     * @param  string  $language  The language of the text, default is auto-detect.
+     * @param  string  $separator  The separator to use between words, default is '-'.
      * @return string The generated slug.
      */
     public function generate($text, $language = DefaultSlugGenerator::LANG_AUTO, $separator = '-');
