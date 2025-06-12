1e3d55090ebef238a9672673be8185989c814ee1
cklei-carly <68525320+cklei-carly@users.noreply.github.com>
2025-06-12 04:39:44 +0000

    Fix styling

--- src/Commands/ImportDefaultDataCommand.php
+++ src/Commands/ImportDefaultDataCommand.php
@@ -38,7 +38,7 @@ class ImportDefaultDataCommand extends Command
                 $step => $this->confirm(
                     "Do you want to skip {$description}?",
                     true
-                )
+                ),
             ];
         })->all();
 
@@ -50,7 +50,7 @@ class ImportDefaultDataCommand extends Command
             if (! method_exists($this, $method)) {
                 throw new RuntimeException("Method {$method} does not exist in " . static::class);
             }
-            
+
             if (array_key_exists($method, $stepCanSkip) && $stepCanSkip[$method]) {
                 $skipAfter = true;
             }
