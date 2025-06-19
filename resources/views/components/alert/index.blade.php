c287047548b6f45218d878b77c2e91448ab57d2e
carly <carly.cheung@solutionforest.net>
2025-06-19 16:37:01 +0800

    fix style

--- resources/views/components/alert/index.blade.php
+++ resources/views/components/alert/index.blade.php
@@ -1,10 +1,7 @@
 <div {{ 
     $attributes->class([
-        'alert-dialog px-4 bg-custom-50 dark:bg-custom-900',
+        'alert-dialog',
         "alert-dialog-{$size}" => in_array($size, ['sm', 'md', 'lg']),
-        'py-2' => $size === 'sm',
-        'py-3' => $size === 'md',
-        'py-4' => $size === 'lg',
     ])->style(\Filament\Support\get_color_css_variables(
         $color,
         shades: [50, 200, 400, 700, 900],
@@ -13,14 +10,12 @@
     x-data="{}"
     x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-alert', package: 'solution-forest/inspirecms'))]"
 >
-    <div class="alert-dialog-body flex items-center h-full truncate">
+    <div class="alert-dialog-body">
         <div class="alert-dialog-icon-ctn">
-            <x-filament::icon :icon="$icon" class="w-6 h-6 text-custom-400 text-custom-200" />
+            <x-filament::icon :icon="$icon" />
         </div>
-        <div class="alert-dialog-content-ctn ml-4 text-sm text-custom-700 dark:text-custom-400">
-            <p>
-                {{ $message }}
-            </p>
+        <div class="alert-dialog-content">
+            <p>{{ $message }}</p>
         </div>
     </div>
 </div>
