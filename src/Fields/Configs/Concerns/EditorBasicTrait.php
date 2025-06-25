4f02859796fcc5d4164e7f37da83c6b6b01ac3d8
carly <carly.cheung@solutionforest.net>
2025-06-25 15:09:58 +0800

    feat: update file attachment fields to use Select component

--- src/Fields/Configs/Concerns/EditorBasicTrait.php
+++ src/Fields/Configs/Concerns/EditorBasicTrait.php
@@ -3,6 +3,7 @@
 namespace SolutionForest\InspireCms\Fields\Configs\Concerns;
 
 use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 
 trait EditorBasicTrait
@@ -18,6 +19,7 @@ trait EditorBasicTrait
     protected static function getEditorBasicTraitComponent($name)
     {
         $defaultDisk = config('filesystems.default', 'public');
+        $disks = config('filesystems.disks', []);
 
         return match ($name) {
             'toolbarButtons' => CheckboxList::make('toolbarButtons')
@@ -25,9 +27,16 @@ trait EditorBasicTrait
                 ->bulkToggleable()
                 ->columns(3),
 
-            'fileAttachmentsDisk' => TextInput::make('fileAttachmentsDisk')->label('Disk')->default($defaultDisk),
+            'fileAttachmentsDisk' => Select::make('fileAttachmentsDisk')->label('Disk')
+                ->default($defaultDisk)
+                ->options(collect($disks)->keys()->mapWithKeys(fn ($disk) => [$disk => $disk])->all()),
             'fileAttachmentsDirectory' => TextInput::make('fileAttachmentsDirectory')->label('Directory'),
-            'fileAttachmentsVisibility' => TextInput::make('fileAttachmentsVisibility')->label('Visibility')->default(config("filesystems.disks.{$defaultDisk}.visibility")),
+            'fileAttachmentsVisibility' => Select::make('fileAttachmentsVisibility')->label('Visibility')
+                ->default($disks[$defaultDisk]['visibility'] ?? 'public')
+                ->options([
+                    'public' => 'Public',
+                    'private' => 'Private',
+                ]),
             default => null,
         };
     }
