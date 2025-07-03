df4fc1aad10caf7ed17b815fd4a2edb432a12819
carly <carly.cheung@solutionforest.net>
2025-07-03 19:27:52 +0800

    bugfix: cannot preview image if the field is media picker

--- src/Fields/Converters/MediaPickerConverter.php
+++ src/Fields/Converters/MediaPickerConverter.php
@@ -18,14 +18,26 @@ class MediaPickerConverter extends BaseConverter
 
         $formattedSourceValue = is_array($value) ? $value : [$value];
 
+        // fetch media assets from the database using primary keys (In Preview mode)
+        $mediaAssetIds = collect($formattedSourceValue)->where(fn ($v) => is_string($v));
+        
+        $mediaAssets = (count($mediaAssetIds) > 0) ? 
+            collect(inspirecms_asset()->findByKeys($mediaAssetIds->all()))->mapWithKeys(fn ($item) => [$item->getKey() => $item]) : 
+            collect();
+
         return collect($formattedSourceValue)
-            ->map(function ($item) {
+            ->map(function ($item) use ($mediaAssets) {
                 if (is_array($item)) {
                     return MediaAssetDto::fromArray($item);
                 } elseif ($item instanceof MediaAsset) {
                     return MediaAssetDto::fromModel($item);
                 } elseif ($item instanceof BaseDto) {
                     return $item;
+                } elseif (is_string($item)) {
+                    $model = $mediaAssets->get($item);
+                    if ($model) {
+                        return MediaAssetDto::fromModel($model);
+                    }
                 }
 
                 return null;
