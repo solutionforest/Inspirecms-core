7168742093e0515cdb0dad322851940413de2242
cklei-carly <carly.cheung@solutionforest.net>
2025-12-09 16:02:36 +0800

    Refactor CSS imports and update theme sources
    Removed redundant @import and @reference statements from alert.css and code-editor.css, consolidating source declarations in theme-dependencies.css. Also cleaned up the .content-sidebar_desktop class in theme.css to remove unused display and position rules. These changes improve maintainability and clarify the source of component styles.

--- resources/views/livewire/content-sidebar.blade.php
+++ resources/views/livewire/content-sidebar.blade.php
@@ -55,7 +55,7 @@
     </x-filament::modal>
 
     <!-- Static sidebar for desktop -->
-    <div class="content-sidebar_desktop">
+    <div class="content-sidebar_desktop hidden lg:fixed lg:top-16 lg:left-0 lg:w-72 lg:flex lg:flex-col z-40 h-svh">
         <div class="flex grow flex-col gap-y-5 overflow-y-auto overflow-x-visible border-e border-gray-200 bg-white py-2 dark:bg-gray-900 dark:border-gray-700">
             <x-inspirecms-support::tree-node.service-side-tree
                 :nodes="$nodes"
